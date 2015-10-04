03eb7ff8230aefbfd6017b424dca8b2a8c8dbf1a
hiroshigiotoku <a20122925@pucp.pe>
2015-10-04 00:21:05 -0500

    Aportando alguito a mi sub-grupo :p

---
+++ src/main/java/pucp/sw2/horario1/vtesis/controllers/AsesorController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pucp.sw2.horario1.vtesis.controllers;
+
+import java.util.logging.Logger;
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author Hiroshi
+ */
+@Controller(value = "asesorController")
+public class AsesorController {
+    
+    
+    private static final Logger log = Logger.getLogger("AsesorController");
+    
+}

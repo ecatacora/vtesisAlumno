f4423dbf7b7ad308e342f0202bee16c86d346027
hiroshigiotoku <a20122925@pucp.pe>
2015-10-03 22:19:54 -0500

    Cambios necesarios para que inicie el proyecto.. Aún hay errores por resolver

--- src/main/java/pucp/sw2/horario1/vtesis/controllers/UserController.java
+++ src/main/java/pucp/sw2/horario1/vtesis/controllers/UserController.java
@@ -5,10 +5,30 @@
  */
 package pucp.sw2.horario1.vtesis.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pucp.sw2.horario1.vtesis.dao.UserDAO;
+
 /**
  *
  * @author Hiroshi
  */
+@Controller(value = "userControler")
 public class UserController {
+    @Autowired
+    UserDAO userDao;
     
+    private static final Logger log = Logger.getLogger("UserController");
+    
+    @RequestMapping(value = "inicio")
+    public String inicial(){
+        log.info("/inicio");
+        return "index.jsp";
+    }
 }
---
+++ src/main/java/pucp/sw2/horario1/vtesis/dao/UserDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pucp.sw2.horario1.vtesis.dao;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Hiroshi
+ */
+@Repository(value = "userDAO")
+public class UserDAO {
+    @Autowired
+    DataSource datasource;
+    
+    
+}

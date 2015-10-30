3acdbec37d4b127c4a8138bdb40b9ea5ae7ffcea
Unknown <Unknown@Unknown-Laptop>
2015-10-30 12:59:41 -0500

    Alumno:Avance de los DTO, tambien informacion necesaria para armar el controlador Alumno

--- src/main/java/pucp/sw2/horario1/vtesis/dto/AlumnoDTO.java
+++ src/main/java/pucp/sw2/horario1/vtesis/dto/AlumnoDTO.java
@@ -10,5 +10,51 @@ package pucp.sw2.horario1.vtesis.dto;
  * @author josesuk
  */
 public class AlumnoDTO {
-    
+
+    private int idPersona;
+    private String nombres;
+    private String apellidos;
+    private String codigo;
+    private String foto;
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
 }

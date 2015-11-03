076284c4760a293fb1af39889b838375d6535732
rosarioorna <rosarioorna@gmail.com>
2015-11-03 17:49:07 -0500

    Archivo con error

--- src/main/java/pucp/sw2/horario1/vtesis/dao/PersonaDAO.java
+++ src/main/java/pucp/sw2/horario1/vtesis/dao/PersonaDAO.java
@@ -43,7 +43,7 @@ public class PersonaDAO {
                 + "e.Enabled "
                 + "from employees e "
                 + "where e.Enabled = 1";
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(hola);
         List<EmployeeBean> employee_list = jdbcTemplate.query(query, new EmployeeMapper());
         return employee_list;
 
